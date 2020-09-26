58d39516d59398a280e2947a695ef54dc53f144e
Cath11 <Cath11@users.noreply.github.com>
2020-09-26 10:36:21 +0800

    cc

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,61 +1,65 @@
-import React,  { useState } from "react";
+import React, { useState } from "react";
 function LoginForm() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
 
-    const [credentials, setCredentials] = useState({
-        username: "",
-        password: "",
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [id]: value,
+    }));
+  };
 
-
-    });
-
-
-    const handleChange = (e) => {
-        const {id, value} =e.target;
-        setCredentials((prevCredentials) => ({
-            ...prevCredentials,
-            [id]:value,
-        }));
-        
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (credentials.username && credentials.password) {
-            fetch(`${process.env.REACT_APP_API_URL}api-token-auth/`,  {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(credentials),
-            }).then((response)=> {
-                console.log(response.json());
-            
-            });
-        }
-    };
-    return(
-        <form>
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input 
-                type="text"
-                id="username" 
-                placeholder="Enter username" 
-                onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input 
-                type="password" 
-                id="password" 
-                placeholder="Enter password" 
-                onChange={handleChange}
-                />
-
-            </div>
-            <button type="submit" onClick={handleSubmit}>Login</button>
-        </form>
+  const postData = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}api-token-auth/`,
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      }
     );
+    return response.json();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (credentials.username && credentials.password) {
+      postData().then((response) => {
+        window.localStorage.setItem("token", response.token);
+      });
+    }
+  };
+  return (
+    <form>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          placeholder="Enter username"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter password"
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Login
+      </button>
+    </form>
+  );
 }
 
 export default LoginForm;
