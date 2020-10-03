983d713f2dfc642fed5fe4e66d0e7de7eb7fa34f
Cath11 <Cath11@users.noreply.github.com>
2020-10-03 12:06:23 +0800

    cc

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 function ProjectPage() {
   const [projectData, setProjectData] = useState({ pledges: [] });
   const { id } = useParams();
+  const history = useHistory();
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
@@ -15,41 +16,47 @@ function ProjectPage() {
       });
   }, [id]);
 
-  // const deleteData = async () => {
-  //   const response = await fetch(
-  //     `${process.env.REACT_APP_API_URL}projects/${id}`,
-  //     {
-  //       method: "delete",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `token ${token}`,
-  //       },
-  //     }
-  //   );
-  //   return response.json();
-  // };
+  const token = window.localStorage.getItem("token");
+
+  const deleteData = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}projects/${id}`,
+
+      {
+        method: "delete",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `token ${token}`,
+        },
+      }
+    );
+    history.push("/");
+  };
 
   return (
     <div>
       <h2>{projectData.title}</h2>
+      <h2>Description: {projectData.description}</h2>
       <h3>Created at: {projectData.date_created}</h3>
-      <h3>Project Status: {`Status: ${projectData.is_open}`}</h3>
-      <h3>Description: {projectData.description}</h3>
-      <h3>Pledges:</h3>
-      <ul>
-        {projectData.pledges.map((pledgeData, key) => {
-          return (
-            <li key={key}>
-              ${pledgeData.amount} from {pledgeData.supporter}
-            </li>
-          );
-        })}
-        {/* <button type="submit" onClick={deleteData}>
-          Delete Project
-        </button> */}
-      </ul>
+      <h3>{`Status: ${projectData.is_open}`}</h3>
+      {projectData.pledges?.length > 0 && (
+        <>
+          <h3>Pledges:</h3>
+          <ul>
+            {projectData.pledges.map((pledgeData, key) => {
+              return (
+                <li key={key}>
+                  ${pledgeData.amount} from {pledgeData.supporter}
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
+      <button type="submit" onClick={deleteData}>
+        Delete Project
+      </button>
     </div>
   );
 }
-
 export default ProjectPage;
