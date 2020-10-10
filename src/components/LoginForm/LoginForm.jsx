206f726540ca3c842c98cb0676ba09cd91f094d5
Cath11 <Cath11@users.noreply.github.com>
2020-10-11 02:59:26 +0800

    c

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -20,7 +20,6 @@ function LoginForm() {
     const response = await fetch(
       `${process.env.REACT_APP_API_URL}api-token-auth/`,
       {
-        mode: 'no-cors',
         method: "post",
         headers: {
           "Content-Type": "application/json",
